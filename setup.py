3a2018da788f594c3e3e66be085e66a0c5911358
erdogant <erdogant@gmail.com>
2023-01-01 16:56:27 +0100

    udpate setup and requirements

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ else:
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-     install_requires=['matplotlib','numpy','pandas','tqdm'],
+     install_requires=['pandas'],
      python_requires='>=3',
      name='dicter',
      version=new_version,
